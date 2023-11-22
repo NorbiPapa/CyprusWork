196702fe8d031b67a00ee1a41ddaef97b73f0934
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2023-11-22 11:37:18 +0100

    HTML & Bootstrap Integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,124 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editinggg&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+    <div className="mrl">
+      <div className="row">
+        <div className="topnav">
+          <a className="active" href="#home">
+            Home
           </a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <div className="row">
+        <div id="default">
+          <h1>Home</h1>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum.
           </p>
-        </a>
+        </div>
+        <div id="news">
+          <h1>News</h1>
+          <p>
+            Tempor orci eu lobortis elementum nibh tellus molestie nunc. Etiam non
+            quam lacus suspendisse faucibus interdum. Sed viverra ipsum nunc aliquet
+            bibendum enim facilisis gravida. Accumsan tortor posuere ac ut. Facilisi
+            etiam dignissim diam quis enim lobortis scelerisque fermentum. Volutpat
+            odio facilisis mauris sit. Eget nunc lobortis mattis aliquam faucibus
+            purus. Eget velit aliquet sagittis id consectetur purus ut. Arcu felis
+            bibendum ut tristique et egestas quis. Convallis convallis tellus id
+            interdum velit laoreet. Risus quis varius quam quisque id diam vel quam.
+            Viverra justo nec ultrices dui. Cras fermentum odio eu feugiat pretium
+            nibh ipsum. Vitae semper quis lectus nulla at volutpat diam ut.Tempor
+            orci eu lobortis elementum nibh tellus molestie nunc. Etiam non quam
+            lacus suspendisse faucibus interdum. Sed viverra ipsum nunc aliquet
+            bibendum enim facilisis gravida. Accumsan tortor posuere ac ut. Facilisi
+            etiam dignissim diam quis enim lobortis scelerisque fermentum. Volutpat
+            odio facilisis mauris sit. Eget nunc lobortis mattis aliquam faucibus
+            purus. Eget velit aliquet sagittis id consectetur purus ut. Arcu felis
+            bibendum ut tristique et egestas quis. Convallis convallis tellus id
+            interdum velit laoreet. Risus quis varius quam quisque id diam vel quam.
+            Viverra justo nec ultrices dui. Cras fermentum odio eu feugiat pretium
+            nibh ipsum. Vitae semper quis lectus nulla at volutpat diam ut.
+          </p>
+        </div>
+        <div id="contact">
+          <h1>Contact</h1>
+          <p>
+            Proin nibh nisl condimentum id venenatis a condimentum. Quam quisque id
+            diam vel quam elementum pulvinar etiam. Sit amet justo donec enim diam
+            vulputate. Dui sapien eget mi proin sed. Senectus et netus et malesuada.
+            Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Et tortor
+            consequat id porta nibh venenatis cras sed felis. Hendrerit dolor magna
+            eget est lorem ipsum dolor sit amet. Magna fermentum iaculis eu non
+            diam. Facilisi nullam vehicula ipsum a. Ac ut consequat semper viverra
+            nam libero justo laoreet. In hendrerit gravida rutrum quisque non tellus
+            orci. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare.
+            Lectus arcu bibendum at varius vel pharetra.Proin nibh nisl condimentum
+            id venenatis a condimentum. Quam quisque id diam vel quam elementum
+            pulvinar etiam. Sit amet justo donec enim diam vulputate. Dui sapien
+            eget mi proin sed. Senectus et netus et malesuada. Tellus rutrum tellus
+            pellentesque eu tincidunt tortor aliquam. Et tortor consequat id porta
+            nibh venenatis cras sed felis. Hendrerit dolor magna eget est lorem
+            ipsum dolor sit amet. Magna fermentum iaculis eu non diam. Facilisi
+            nullam vehicula ipsum a. Ac ut consequat semper viverra nam libero justo
+            laoreet. In hendrerit gravida rutrum quisque non tellus orci. Mattis
+            ullamcorper velit sed ullamcorper morbi tincidunt ornare. Lectus arcu
+            bibendum at varius vel pharetra.
+          </p>
+        </div>
+        <div id="about">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Posuere urna nec
+            tincidunt praesent semper feugiat nibh sed pulvinar. Dictum sit amet
+            justo donec. Scelerisque eleifend donec pretium vulputate sapien. Varius
+            duis at consectetur lorem donec massa sapien faucibus et. Venenatis
+            lectus magna fringilla urna porttitor rhoncus dolor purus non. Fermentum
+            dui faucibus in ornare quam viverra orci sagittis. Tempus imperdiet
+            nulla malesuada pellentesque elit eget gravida cum sociis. Fames ac
+            turpis egestas integer. Pretium quam vulputate dignissim suspendisse in.
+            Quisque egestas diam in arcu cursus. Viverra orci sagittis eu volutpat
+            odio facilisis mauris sit. Molestie a iaculis at erat pellentesque
+            adipiscing commodo elit. Arcu cursus euismod quis viverra nibh. Odio
+            euismod lacinia at quis risus sed vulputate. Egestas sed sed risus
+            pretium quam vulputate dignissim. Tempus iaculis urna id volutpat. Elit
+            pellentesque habitant morbi tristique senectus et netus et. Amet mattis
+            vulputate enim nulla aliquet porttitor. Ut ornare lectus sit amet
+            est.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere urna
+            nec tincidunt praesent semper feugiat nibh sed pulvinar. Dictum sit amet
+            justo donec. Scelerisque eleifend donec pretium vulputate sapien. Varius
+            duis at consectetur lorem donec massa sapien faucibus et. Venenatis
+            lectus magna fringilla urna porttitor rhoncus dolor purus non. Fermentum
+            dui faucibus in ornare quam viverra orci sagittis. Tempus imperdiet
+            nulla malesuada pellentesque elit eget gravida cum sociis. Fames ac
+            turpis egestas integer. Pretium quam vulputate dignissim suspendisse in.
+            Quisque egestas diam in arcu cursus. Viverra orci sagittis eu volutpat
+            odio facilisis mauris sit. Molestie a iaculis at erat pellentesque
+            adipiscing commodo elit. Arcu cursus euismod quis viverra nibh. Odio
+            euismod lacinia at quis risus sed vulputate. Egestas sed sed risus
+            pretium quam vulputate dignissim. Tempus iaculis urna id volutpat. Elit
+            pellentesque habitant morbi tristique senectus et netus et. Amet mattis
+            vulputate enim nulla aliquet porttitor. Ut ornare lectus sit amet est.
+          </p>
+        </div>
       </div>
-    </main>
+    </div>
+
   )
 }
