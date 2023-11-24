f90b9e6226396dc6f575396840ea67c3cfaeadc3
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2023-11-24 12:22:21 +0100

    Working on login and conditional Logout button

---
+++ app/app.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+    isLoggedIn: false,
+    login: () => {},
+    logout: () => {}
+  });
+
+  type AuthProviderProps = {
+    children: React.ReactNode;
+  };
+  
+  export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+    useEffect(() => {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        setIsLoggedIn(true);
+      }
+    }, []);
+  
+    const login = () => {
+      localStorage.setItem('authToken', 'your-token');
+      setIsLoggedIn(true);
+    };
+  
+    const logout = () => {
+      localStorage.removeItem('authToken');
+      setIsLoggedIn(false);
+    };
+  
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+        {children}
+        </AuthContext.Provider>
+    );      
+  };
+  
--- hashpassword.js
+++ hashpassword.js
@@ -1,8 +1,11 @@
 const bcrypt = require('bcrypt');
 
-const password = 'admin';
+const password = 'Ship423*';
 const saltRounds = 10;
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
   console.log("Hashed Password:", hash);
 });
+
+// shipconadmin
+// Ship423*
