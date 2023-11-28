60b30507b7aa79779f049727697b3e4dd26b61a8
tthzsombor <zsombortoooth@gmail.com>
2023-11-28 12:18:09 +0100

    script

--- backend/design.js
+++ backend/design.js
@@ -1,14 +1,20 @@
-var prevScrollpos = window.pageYOffset;
-
-window.onscroll = function() {
-
-  var currentScrollPos = window.pageYOffset;
-
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("topnav").style.top = "0";
-  } else {
-    document.getElementById("topnav").style.top = "-100%";
+function HeaderEffect()
+{
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("topnav").style.top = "0";
+    } else {
+      document.getElementById("topnav").style.top = "-100%";
+    }
+    prevScrollpos = currentScrollPos;
   }
-
-  prevScrollpos = currentScrollPos;
 }
+
+function init()
+{
+HeaderEffect();
+}
+
+document.addEventListener('DOMContentLoaded', init)
